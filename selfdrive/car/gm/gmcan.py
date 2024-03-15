19c198ea27e19ba273e903f6b22bab3c35bd74a2
garrettpall <76917194+garrettpall@users.noreply.github.com>
2024-03-15 17:03:38 -0400

    Add 1.01 factor to Vego

--- selfdrive/car/gm/gmcan.py
+++ selfdrive/car/gm/gmcan.py
@@ -262,7 +262,7 @@ def create_gm_acc_spam_command(packer, controller, CS, slcSet, bus, Vego, frogpi
     if slcSet + 5 < Vego * CV.MS_TO_MPH:
       slcSet = slcSet - 10
   else:
-    slcSet = int(round(Vego * CV.MS_TO_MPH + accel * 11.2))
+    slcSet = int(round(Vego * CV.MS_TO_MPH * 1.01 + accel * 11.2)) # 1.01 factor to match cluster speed better
   
   if slcSet <= int(math.floor((speedSetPoint - 1)/5.0)*5.0) and speedSetPoint > 20:
     cruiseBtn = CruiseButtons.DECEL_SET
