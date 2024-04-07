1359b9767187e51a6c8e5cdc673ca2c8ed3371f5
luckylz2git <8618993@qq.com>
2024-04-07 23:54:16 +0800

    TEST_STEER

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -477,7 +477,6 @@ void ExperimentalButton::paintEvent(QPaintEvent *event) {
   // Custom steering wheel icon
   engage_img = wheelImages[wheelIcon];
   QPixmap img = wheelIcon ? engage_img : (experimental_mode ? experimental_img : engage_img);
-  img = experimental_img;
   QMovie *gif = wheelImagesGif[wheelIconGif];
 
   QColor background_color = wheelIcon != 0 && !isDown() && engageable ?
