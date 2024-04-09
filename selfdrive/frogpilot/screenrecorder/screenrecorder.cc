731afa3dfdc0242682fedec41a600f0aee3ea251
luckylz2git <8618993@qq.com>
2024-04-09 23:32:18 +0800

    recorder_btn

--- selfdrive/frogpilot/screenrecorder/screenrecorder.cc
+++ selfdrive/frogpilot/screenrecorder/screenrecorder.cc
@@ -45,7 +45,8 @@ void ScreenRecorder::paintEvent(QPaintEvent *event) {
   QPainter p(this);
   p.setRenderHint(QPainter::Antialiasing);
 
-  QRect fullRect(0, 0, 192 / 2, 192 / 2);
+  //QRect fullRect(0, 0, 192 / 2, 192 / 2);
+  QRect fullRect(0, 192 / 2, 192 / 2, 192 / 2);
   QColor outerColor = recording ? recording_color : QColor::fromRgbF(1, 1, 1, 1);
   int outerRedWidth = fullRect.width() * 0.05;
 
