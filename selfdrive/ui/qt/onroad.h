fceedaf31dc0fa31e6317450ee6fe84ee8b81ecf
luckylz2git <8618993@qq.com>
2024-04-10 21:16:55 +0800

    showRecordButton

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -195,7 +195,6 @@ private:
   bool has_us_speed_limit = false;
   bool has_eu_speed_limit = false;
   bool v_ego_cluster_seen = false;
-  bool showRecordButton = false;
   int status = STATUS_DISENGAGED;
   std::unique_ptr<PubMaster> pm;
 
@@ -315,6 +314,7 @@ signals:
   void mapPanelRequested();
 
 private:
+  bool showRecordButton = false;
   void paintEvent(QPaintEvent *event);
   void mousePressEvent(QMouseEvent* e) override;
   OnroadAlerts *alerts;
