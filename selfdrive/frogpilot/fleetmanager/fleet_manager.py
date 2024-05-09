4c0db83178b058403cc4e0f47da91c3230f690e1
luckylz2git <8618993@qq.com>
2024-05-09 20:04:36 +0800

    UDP TEST

--- selfdrive/frogpilot/fleetmanager/fleet_manager.py
+++ selfdrive/frogpilot/fleetmanager/fleet_manager.py
@@ -453,7 +453,7 @@ def main():
   except Exception:
     cloudlog.exception("fleet_manager: failed to set core affinity")
   #UDP测试
-  #threading.Thread(target=fleet.udp_send_message).start()
+  threading.Thread(target=fleet.udp_send_message).start()
   app.secret_key = secrets.token_hex(32)
   app.run(host="0.0.0.0", port=8082)
 
