996354ee234080f1b15ba324c0c13a89cb487d0d
luckylz2git <8618993@qq.com>
2024-09-01 12:00:38 +0800

    auto_resume

--- selfdrive/frogpilot/ui/vehicle_settings.cc
+++ selfdrive/frogpilot/ui/vehicle_settings.cc
@@ -127,7 +127,7 @@ FrogPilotVehiclesPanel::FrogPilotVehiclesPanel(SettingsWindow *parent) : FrogPil
     {"OnStarGPS", tr("Use OnStar GPS Signal"), "Use GM OnStar GPS Signal, instead of built-in GPS Receiver.", ""},
     {"GearNumber", tr("Show Gear Number"), "Use speed limit widget to show current gear number.", ""},
     {"UseRedPanda", tr("Use External Red Panda"), tr("Use external red panda to connect can bus."), ""},
-    {"CruiseAutoResume", tr("Cruise Auto Resume"), tr("Enable Auto Resume when Cruise On, Set Speed 25 KPH and Aggressive following distance."), ""},
+    {"CruiseAutoResume", tr("Cruise Auto Resume"), tr("Enable Auto Resume when Adapted Cruise Control On, Set Speed 25 kph and Aggressive following distance."), ""},
     {"EVTable", tr("EV Lookup Tables"), "Smoothen out the gas and brake controls for EV vehicles.", ""},
     {"GasRegenCmd", tr("GM Truck Gas Tune"), "Increase acceleration and smoothen brake to stop. For use on Silverado/Sierra only.", ""},
     {"LongPitch", tr("Long Pitch Compensation"), "Reduce speed and acceleration error for greater passenger comfort and improved vehicle efficiency.", ""},
