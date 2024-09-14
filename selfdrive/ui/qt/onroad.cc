c9aceaf96b8843f6d3155f3db49a6d8f34bece50
luckylz2git <8618993@qq.com>
2024-09-14 20:27:05 +0800

    png

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1905,7 +1905,8 @@ PersonalityButton::PersonalityButton(QWidget *parent) : QPushButton(parent), sce
 
   // Configure the profile vector
   profile_data = {
-    {QPixmap("../frogpilot/assets/other_images/aggressive_new.png"), "Aggressive"},
+    //{QPixmap("../frogpilot/assets/other_images/aggressive_new.png"), "Aggressive"},
+    {QPixmap("../frogpilot/assets/other_images/aggressive_new2.png"), "Aggressive"},
     {QPixmap("../frogpilot/assets/other_images/standard_new.png"), "Standard"},
     {QPixmap("../frogpilot/assets/other_images/relaxed_new.png"), "Relaxed"}
   };
