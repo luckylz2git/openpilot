c85efb14353ae509f5bcc6bea8775e6460d0e4a8
luckylz2git <8618993@qq.com>
2024-10-30 12:18:19 +0800

    hide speed

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1549,6 +1549,9 @@ void AnnotatedCameraWidget::updateFrogPilotWidgets(QPainter &p) {
     }
     scene.screen_recorder_toggle = 0;
   }
+
+  experimental_btn->setVisible(!scene.hide_speed || scene.screen_recorder);
+
   // Update the turn signal animation images upon toggle change
   if (customSignals != scene.custom_signals || currentHolidayTheme != scene.current_holiday_theme) {
     currentHolidayTheme = scene.current_holiday_theme;
