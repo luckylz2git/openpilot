1122f7ffaec4b68a57d829d55b9ee9f5fe0eb983
luckylz2git <8618993@qq.com>
2025-02-27 09:36:21 +0800

    AutoResumeDistance

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -666,7 +666,7 @@ class Controls:
       lead = self.sm['radarState'].leadOne
       lead_distance = lead.dRel
       lead_departing = lead_distance - self.previous_lead_distance > 0.5 and self.previous_lead_distance != 0 and CS.standstill
-      self.previous_lead_distance = lead_distance
+      # self.previous_lead_distance = lead_distance
 
       lead_departing &= not CS.gasPressed
       lead_departing &= lead.vLead > 1
@@ -679,7 +679,7 @@ class Controls:
           # read param only when lead_departing = true
           cruise_auto_resume = self.params.get_bool("CruiseAutoResume") and self.params_memory.get_bool("ESP32HasIP") #auto_resume
           conversion = 1 if self.is_metric else CV.FOOT_TO_METER
-          cruise_auto_resume &= lead_distance <= self.params.get_int("AutoResumeDistance")*conversion
+          cruise_auto_resume &= self.previous_lead_distance <= self.params.get_int("AutoResumeDistance")*conversion
           long_personality = self.params.get_int("LongitudinalPersonality") == 0
           if long_personality and cruise_auto_resume and self.state == State.enabled and not CS.brakePressed and self.v_cruise_helper.v_cruise_cluster_kph < 24.0:
             self.params_memory.put_bool("ESP32AutoResume", True)
@@ -688,6 +688,8 @@ class Controls:
             self.events.add(EventName.leadDeparting)
         else:
           self.events.add(EventName.leadDeparting)
+      
+      self.previous_lead_distance = lead_distance
 
     # Speed limit changed alert
     if self.speed_limit_alert or self.speed_limit_confirmation:
