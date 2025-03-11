286c4b9a44d9576085b9267edbf39f28e891dd05
luckylz2git <8618993@qq.com>
2025-03-11 17:28:00 +0800

    SpeedMap

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -293,7 +293,7 @@ private:
   std::unordered_map<int, std::tuple<QString, int, QColor, std::map<double, QBrush>>> holidayThemeConfiguration;
   std::unordered_map<int, std::tuple<QString, int, QColor, std::map<double, QBrush>>> themeConfiguration;
   //SpeedMap
-  std::unordered_map<int, float> accSpeeds;
+  std::map<int, float> accSpeedMaps;
   std::vector<QPixmap> signalImgVector;
 
   QTimer *animationTimer;
