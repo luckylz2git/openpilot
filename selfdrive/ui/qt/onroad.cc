67d3a973a64fbef5773f3e5fd427ab5a537d10ad
luckylz2git <8618993@qq.com>
2025-03-12 19:15:18 +0800

    SpeedMap

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -753,7 +753,7 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
   //setSpeedStr = QString::number(getAccSpeedActual(25),'f',2);
   if (is_metric) {
     //测试
-    speedStr = is_cruise_set ? QString::number(setSpeed, 'f', 2) + " | " + QString::number(cruiseAdjustment, 'f', 2) : "-";
+    //speedStr = is_cruise_set ? QString::number(setSpeed, 'f', 2) + " | " + QString::number(cruiseAdjustment, 'f', 2) : "-";
     setSpeedStr = is_cruise_set ? QString::number(getAccSpeedDisplay(setSpeed - cruiseAdjustment)) : "–";
   } else {
     setSpeedStr = is_cruise_set ? QString::number(std::nearbyint(setSpeed - cruiseAdjustment)) : "–";
