57276443fdc2881a3f594a73932b463daeeec9b0
luckylz2git <8618993@qq.com>
2025-03-17 13:57:06 +0800

    v_cruise_kph_set

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -41,7 +41,8 @@ CRUISE_INTERVAL_SIGN = {
 class VCruiseHelper:
   def __init__(self, CP):
     self.CP = CP
-    self.v_cruise_has_set = False
+    self.v_cruise_has_set = 0
+    self.v_cruise_kph_set = 0
     self.v_cruise_kph = V_CRUISE_UNSET
     self.v_cruise_cluster_kph = V_CRUISE_UNSET
     self.v_cruise_kph_last = 0
@@ -60,10 +61,11 @@ class VCruiseHelper:
   def update_v_cruise(self, CS, enabled, is_metric, speed_limit_changed, frogpilot_variables):
     #SpeedMap
     self.speed_map.enable_acc_speed_maps(frogpilot_variables.use_acc_speed_maps)
-    if self.v_cruise_has_set:
-      if abs(CS.cruiseState.speed * CV.MS_TO_KPH - self.v_cruise_kph) <= 0.5:
-        self.v_cruise_kph = self.speed_map.get_acc_speed_actual(self.speed_map.get_acc_speed_display(CS.cruiseState.speed * CV.MS_TO_KPH))
-      self.v_cruise_has_set = False
+    if self.v_cruise_has_set > 0:
+      if self.v_cruise_has_set == 1 and self.v_cruise_kph_set == self.v_cruise_kph:
+        if abs(self.speed_map.get_acc_speed_display(CS.cruiseState.speed * CV.MS_TO_KPH) - self.speed_map.get_acc_speed_display(self.v_cruise_kph)) == 1:
+          self.v_cruise_kph = self.speed_map.get_acc_speed_actual(self.speed_map.get_acc_speed_display(CS.cruiseState.speed * CV.MS_TO_KPH))
+      self.v_cruise_has_set -= 1
 
     self.v_cruise_kph_last = self.speed_map.get_acc_speed_actual(self.speed_map.get_acc_speed_display(self.v_cruise_kph))  
 
@@ -162,7 +164,8 @@ class VCruiseHelper:
     if CS.gasPressed and button_type in (ButtonType.decelCruise, ButtonType.setCruise):
       #self.v_cruise_kph = max(self.v_cruise_kph, CS.vEgo * CV.MS_TO_KPH)
       self.v_cruise_kph = self.speed_map.get_acc_speed_actual(self.speed_map.get_acc_speed_display(max(self.v_cruise_kph, CS.vEgo * CV.MS_TO_KPH)))
-      self.v_cruise_has_set = frogpilot_variables.use_acc_speed_maps
+      self.v_cruise_has_set = 7 if frogpilot_variables.use_acc_speed_maps else 0
+      self.v_cruise_kph_set = self.v_cruise_kph
       self.params_memory.put_int("SetSpeedTestKey", 1) #ACC Set Speed Test Key
 
     #保留2位小数
@@ -206,7 +209,8 @@ class VCruiseHelper:
           # Use fixed initial set speed from mode etc.
           #self.v_cruise_kph = int(round(clip(CS.vEgo * CV.MS_TO_KPH, initial, V_CRUISE_MAX)))
           self.v_cruise_kph = self.speed_map.get_acc_speed_actual(self.speed_map.get_acc_speed_display(clip(CS.vEgo * CV.MS_TO_KPH, initial, V_CRUISE_MAX)))
-          self.v_cruise_has_set = frogpilot_variables.use_acc_speed_maps #and self.v_cruise_kph > initial
+          self.v_cruise_has_set = 7 if frogpilot_variables.use_acc_speed_maps else 0
+          self.v_cruise_kph_set = self.v_cruise_kph
           self.params_memory.put_int("SetSpeedTestKey", 2) #ACC Set Speed Test Key
 
       self.v_cruise_cluster_kph = self.v_cruise_kph
@@ -225,7 +229,8 @@ class VCruiseHelper:
         # Use fixed initial set speed from mode etc.
         #self.v_cruise_kph = int(round(clip(CS.vEgo * CV.MS_TO_KPH, initial, V_CRUISE_MAX)))        
         self.v_cruise_kph = self.speed_map.get_acc_speed_actual(self.speed_map.get_acc_speed_display(clip(CS.vEgo * CV.MS_TO_KPH, initial, V_CRUISE_MAX)))
-        self.v_cruise_has_set = frogpilot_variables.use_acc_speed_maps #and self.v_cruise_kph > initial
+        self.v_cruise_has_set = 7 if frogpilot_variables.use_acc_speed_maps else 0
+        self.v_cruise_kph_set = self.v_cruise_kph
         self.params_memory.put_int("SetSpeedTestKey", 3) #ACC Set Speed Test Key
             
     self.v_cruise_cluster_kph = self.v_cruise_kph
